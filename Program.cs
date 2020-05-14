2bb2caeda568f62077d57e4a1bcae9c6a4d3400a
adriano.exe <58786675+adriano@users.noreply.github.com>
2020-05-14 19:12:11 -0300

    novo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace turma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int turma;
+            int qtdalunos;
+
+            Console.Write("Quantas turmas? ");
+            turma = Convert.ToInt32(Console.ReadLine());
+
+            int[] Listaturma = new int[turma];
+
+            for (int i = 0; i < turma; i++)
+            {
+                Console.Write("Quantos alunos tem na turma" + i + ": ");
+                turma = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+        }
+    }
+}
