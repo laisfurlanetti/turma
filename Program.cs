0bd0e3ab75a8bcc8b6aace3ce01ed62665a5db6a
adriano.exe <58786675+adriano@users.noreply.github.com>
2020-05-14 23:41:55 -0300

    atualizado

--- Program.cs
+++ Program.cs
@@ -11,20 +11,36 @@ namespace turma
         static void Main(string[] args)
         {
             int turma;
-            int qtdalunos;
+            int totalalunos;
+           
 
             Console.Write("Quantas turmas? ");
             turma = Convert.ToInt32(Console.ReadLine());
 
-            int[] Listaturma = new int[turma];
+            int[] listaaluno = new int[turma];
 
             for (int i = 0; i < turma; i++)
             {
-                Console.Write("Quantos alunos tem na turma" + i + ": ");
-                turma = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Quantos alunos tem na turma " + (i+1) + ": ");
+                var qtdalunos = Convert.ToInt32(Console.ReadLine());
+
+                if (qtdalunos <= 40)
+                {
+                    listaaluno[i] = qtdalunos;
+                }
+                else
+                {
+                   while (qtdalunos > 40)
+                    {
+                        Console.Write("Quantidade de aluno maior que o informado, digite novamente: ");
+                        qtdalunos = Convert.ToInt32(Console.ReadLine());
+                    }
+                    listaaluno[i] = qtdalunos;                    
+                }                
             }
-
-
+            var media = listaaluno.Average();
+            Console.Write("A media e: " + media);
+            Console.Read();
         }
     }
 }
